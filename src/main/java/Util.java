9a7cc0d8a4fd795d047596eb57bd955e0e53901a
gaspargabor <gaspar.gabor3@gmail.com>
2021-03-04 09:40:21 +0100

    app starts without reset

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,2 +1,23 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Util {
+    public AndroidDriver<AndroidElement> createDriver() throws MalformedURLException {
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability(MobileCapabilityType.DEVICE_NAME,"emulator-5554");
+        dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
+        dc.setCapability("appPackage", "com.quizlet.quizletandroid");
+        dc.setCapability("appActivity", "com.quizlet.quizletandroid.ui.RootActivity");
+        dc.setCapability("noReset", "true");
+        return new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), dc);
+    }
+
+    public void quit(AndroidDriver driver){
+        driver.quit();
+    }
 }
--- src/main/java/pages/StarterPage.java
+++ src/main/java/pages/StarterPage.java
@@ -1,4 +1,24 @@
 package pages;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
 public class StarterPage {
+
+    private AndroidDriver driver;
+
+    @AndroidFindBy(id = "com.quizlet.quizletandroid:id/account_login_button")
+    AndroidElement signInButton;
+
+    public StarterPage(AndroidDriver<AndroidElement> driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    public void clickSignIn(){
+        signInButton.click();
+    }
 }
